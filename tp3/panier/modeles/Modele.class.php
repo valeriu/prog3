2c6f7b05702d5a80aaeaa2b20396793dc947f0b1
Valeriu Tihai <valeriu@tihai.md>
2014-01-06 16:52:00 -0500

    methodes

--- tp3/panier/modeles/Modele.class.php
+++ tp3/panier/modeles/Modele.class.php
@@ -12,31 +12,37 @@
  */
 class Modele {
 	
-    
-	function __construct ()
-	{
-		
+    //Connexion à la base
+	function bdconnect() {
+		// Data base config
+		$dsn = "mysql:host=localhost;dbname=ex07";
+		$user = "root";
+		$pass = "";
+
+		$id = new PDO($dsn, $user, $pass);
+		$id->exec('SET NAMES UTF8');
+		return $id;
 	}
 	
-	function __destruct ()
-	{
-		
+	// Enregistrer les données dans la base de données
+	public function setCommande($email, $data, $produitbd, $total){
+		$id = $this->bdconnect();
+		 
+		$req = "INSERT INTO panier (email, date, produis, total) VALUES ('".$email."', '".$data."', '".$produitbd."', '".$total."')";
+		$done = $id->exec($req);
+		return $req;
 	}
-	
-		
-	/**
-	 * @access public
-	 * @return Array
-	 */
-	public function getData() 
-	{
-				
-		
-		
+
+	//Renvoie des informations sur les produits de base de données
+	public function getListProduis($email){
+		$id = $this->bdconnect();
+
+		$req = $id->query("SELECT * FROM panier WHERE email = '".$email."' ORDER BY date DESC");
+		$result = $req->fetchAll(PDO::FETCH_ASSOC);
+
+		return $result;
 	}
+
 }
-
-
-
 
 ?>
